33515b0081d04fde8c3a51d5efc2bd10570b3cb5
BigDaddyDuuk <juniorduc44@gmail.com>
2025-08-04 20:16:40 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/elitetrailer-reimagined-nc/',
+export default defineConfig({
+  base: '/',
   build: {
     outDir: 'docs',
   },
